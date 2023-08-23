5b4fb6656aca808815c314562e623aa0ee89c4ea
renzo cardenas <theaoxd117@gmail.comm>
2023-08-23 00:50:58 -0500

    hooks

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import Header from './components/header/Header';
 import Formulario from './components/Formulario/Formulario';
 import MiOrg from './components/MiOrg';
+import { useState } from 'react';
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar]=useState(true)
+  //Ternario --> condicion? seMuestra :  noSeMuestra
+  const cambiarMostrar=()=>{
+    actualizarMostrar(!mostrarFormulario)
+  }
   return (
     <div>
-      <Header />
-      <Formulario />
-      <MiOrg/>
+      <Header/>
+      {mostrarFormulario ? <Formulario /> : <></>}
+      <MiOrg cambiarMostrar={cambiarMostrar}/>
     </div>
-  );
+  ); 
 }
 
 export default App;
--- src/components/MiOrg/index.js
+++ src/components/MiOrg/index.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import "./MiOrg.css"
-const MiOrg=()=>{
+const MiOrg=(props)=>{
+    //hooks
+    //useState
+    //const[nombreVariable, actualizarNombre]=useSate(true)
+    console.log(props)
+    // const[mostrar, actualizarMostrar]=useState(true)
+    // const manejarClick=()=>{
+    //     console.log("mostrar/ocultar" ,!mostrar);
+    //     actualizarMostrar(!mostrar);
+    //};
         return(
             <section className="orgSection">
                 <h3 className="tittle">Mi organizacion</h3>
-                <img src="/img/boton_add.png" alt="add"/>
+                <img src="/img/boton_add.png" alt="add" onClick={props.cambiarMostrar}/>
             </section>
         )
 }
